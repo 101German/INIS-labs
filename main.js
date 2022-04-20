2f3785469e1352db553332838a39fb9e0c2ce70b
Ryhor Samoshkin <ryhor.samoshkin@innowise-group.com>
2022-04-20 15:14:39 +0300

    Add Lab1

--- main.js
+++ main.js
@@ -2,15 +2,92 @@
 //       You can access this variable here, and should use this variable here to build your webpages
 
 let initProducts = () => {
-    // To see the shirts object, run:
-    // console.log(shirts);
+    const productsContainer = document.getElementById('products');
 
-    // Your Code Here
+    for (let i = 0; i < shirts.length; i++) {
+        const shirt = shirts[i];
+        const productCard = document.createElement("div");
+        productCard.classList.add("product_card")
+
+        const image = document.createElement("img");
+        image.src = shirt.colors.white.front;
+        image.alt = shirt.name;
+        image.classList.add("product_image");
+        productCard.append(image);
+
+        const name = document.createElement("h3");
+        name.innerText = shirt.name;
+        name.classList.add("product_name");
+        productCard.append(name);
+
+        const colorCount = document.createElement("h4");
+        let count = Object.keys(shirt.colors).length;
+        colorCount.innerText = `Available in ${count} colors`
+        colorCount.classList.add("product_color_info");
+        productCard.append(colorCount);
+
+        const productCardFooter = document.createElement("div");
+        productCardFooter.classList.add("product_card_footer")
+
+        const quickViewButton = document.createElement("button");
+        quickViewButton.textContent = "Quick View";
+        const seePageButton = document.createElement("button");
+        seePageButton.textContent = "See Page";
+        const pageLink = document.createElement("a");
+        pageLink.href = `details.html?id=${i}`;
+        pageLink.appendChild(seePageButton);
+        productCardFooter.appendChild(pageLink);
+        productCardFooter.appendChild(quickViewButton);
+        productCard.append(productCardFooter);
+
+        productsContainer.appendChild(productCard);
+    }
 };
 
 let initDetails = () => {
-    // To see the shirts object, run:
-    // console.log(shirts);
 
-    // Your Code Here
+    var shirtColor = "white";
+    var shirtSide = "front";
+
+    const id = new URLSearchParams(window.location.search).get('id');
+    var shirt = shirts[id];
+
+    const name = document.getElementById("tshirt_name");
+    name.textContent = shirt.name;
+
+    const img = document.getElementById("tshirt_img");
+    img.src = shirt.colors[shirtColor][shirtSide];
+    img.alt = shirt.name;
+
+    const price = document.getElementById("tshirt_prc");
+    price.innerText = shirt.price;
+    const description = document.getElementById("tshirt-desc");
+    description.innerText = shirt.description;
+
+    const frontBtn = document.getElementById("front_btn");
+    frontBtn.addEventListener('click', () => {
+        shirtSide = "front";
+        img.src = shirt.colors[shirtColor][shirtSide];
+    });
+
+    const backBtn = document.getElementById("back_btn");
+    backBtn.addEventListener('click', () => {
+        shirtSide = "back";
+        img.src = shirt.colors[shirtColor][shirtSide];
+    });
+
+    const colorMenu = document.getElementById("clr_menu");
+    const colors = Object.keys(shirt.colors);
+    for (let i = 0; i < colors.length; i++) {
+        const color = colors[i];
+        const colorBtn = document.createElement("button");
+        colorBtn.textContent = color;
+        colorBtn.style.backgroundColor = color;
+        colorBtn.addEventListener('click', () => {
+            shirtColor = color;
+            img.src = shirt.colors[shirtColor][shirtSide];
+        })
+
+        colorMenu.appendChild(colorBtn)
+    }
 };
--- shirts.js
+++ shirts.js
@@ -25,6 +25,63 @@ var shirts = [{
         "back": "shirt_images/default-m-back.png"
     }
 },
+
+{
+    "name": "Beep Boop",
+    "description": "Once upon a time, a mighty guide guarded the intersection of Forbes and Morewood, and would dutifully direct distracted college students when it was safe to cross the street. Its voice was soothing, strong, and steady. Its name was beep boop.",
+    "price": "$19.99",
+    "colors": {
+        "white": {
+            "front": "shirt_images/beepboop-white-front.png",
+            "back": "shirt_images/beepboop-white-back.png"
+        },
+        "blue": {
+            "front": "shirt_images/beepboop-blue-front.png",
+            "back": "shirt_images/beepboop-blue-back.png"
+        },
+        "pink": {
+            "front": "shirt_images/beepboop-pink-front.png",
+            "back": "shirt_images/beepboop-pink-back.png"
+        },
+        "red": {
+            "front": "shirt_images/beepboop-red-front.png",
+            "back": "shirt_images/beepboop-red-back.png"
+        }
+    },
+    "default": {
+        "front": "shirt_images/default-m-front.png",
+        "back": "shirt_images/default-m-back.png"
+    }
+},
+
+{
+    "name": "Beep Boop",
+    "description": "Once upon a time, a mighty guide guarded the intersection of Forbes and Morewood, and would dutifully direct distracted college students when it was safe to cross the street. Its voice was soothing, strong, and steady. Its name was beep boop.",
+    "price": "$19.99",
+    "colors": {
+        "white": {
+            "front": "shirt_images/beepboop-white-front.png",
+            "back": "shirt_images/beepboop-white-back.png"
+        },
+        "blue": {
+            "front": "shirt_images/beepboop-blue-front.png",
+            "back": "shirt_images/beepboop-blue-back.png"
+        },
+        "pink": {
+            "front": "shirt_images/beepboop-pink-front.png",
+            "back": "shirt_images/beepboop-pink-back.png"
+        },
+        "red": {
+            "front": "shirt_images/beepboop-red-front.png",
+            "back": "shirt_images/beepboop-red-back.png"
+        }
+    },
+    "default": {
+        "front": "shirt_images/default-m-front.png",
+        "back": "shirt_images/default-m-back.png"
+    }
+},
+
 {
     "name": "Car",
     "description": "As you move in to campus, one of the first memories so many students have is driving up to their dorm, unloading their bags, and moving in. How do they arrive to campus? By car, of course!",
@@ -89,4 +146,20 @@ var shirts = [{
         "front": "shirt_images/default-m-front.png",
         "back": "shirt_images/default-m-back.png"
     }
-}];
+},
+{
+    "name": "BSUIR",
+    "description": "BSUIR mission is to train engineers and scientists capable of generating and implementing innovative ideas, creating competitive high technology products in the spheres of computer science and electronics.",
+    "price": "$6.99",
+    "colors": {
+        "white": {
+            "front": "shirt_images/bsuir-white-front.png",
+            "back": "shirt_images/bsuir-white-back.png"
+        }
+    },
+    "default": {
+        "front": "shirt_images/default-m-front.png",
+        "back": "shirt_images/default-m-back.png"
+    }
+}
+];
